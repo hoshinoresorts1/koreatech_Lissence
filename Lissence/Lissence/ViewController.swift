1e5147ea253b9ee541134f35386dc28437dd9481
ussagi <usagi198444@gmail.com>
2026-01-19 22:46:55 +0900

    chore: 프로젝트 초기 세팅

---
+++ Lissence/Lissence/ViewController.swift
@@ -0,0 +1,14 @@
+/// 뷰 컨트롤러(메인 코드) 입니다.
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
